4401ca7e365437c00117ea7171ee0fe877355e69
clefcodex <iwewale1@gmail.com>
2021-08-19 12:15:10 +0100

    Updated Settings

--- try_things/settings.py
+++ try_things/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-46y#g@-$sqbkmg9&rxtkms-2orpfh(u5@#m2*9wmnf1fm3oxp0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'tryresume.herokuapp.com']
 
 
 # Application definition
